6d8eeece45e344a3f0d5499ec3199d31f31e007c
Giovanny Bejarano <giovanny10@msn.com>
2023-12-16 16:22:29 -0500

    23.2SQLAlchemy Associations

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template, redirect,flash, session ,json,jsonify
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 from flask_debugtoolbar import DebugToolbarExtension
 
 
@@ -11,7 +11,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 connect_db(app)
-
+""" db.drop_all()
+db.create_all()
+ """
 @app.route("/")
 def home_page():
   users =User.query.all()
@@ -68,3 +70,52 @@ def update_user_post (user_id):
   return render_template("userdetail.html", user=user)
    
 
+@app.route("/users/<int:user_id>/posts/new",methods=["GET"])
+def show_form_to_add_a_post  (user_id):
+  userid = user_id
+  user = User.query.get_or_404(user_id)
+  user_name = f"{user.first_name} {user.last_name}"
+  return render_template("postform.html",userid=userid, user_name = user_name) 
+
+@app.route("/users/<int:user_id>/posts/new",methods=["POST"])
+def add_post  (user_id):
+  tittle = request.form['tittle']
+  content = request.form['content']
+  post = Post(tittle=tittle, content=content, user_id=user_id)
+  db.session.add(post)
+  db.session.commit()
+  print(f"Post  created  with id {post.id}")
+  user = User.query.get_or_404(user_id)
+  return render_template("userdetail.html", user=user) 
+
+@app.route("/posts/<int:post_id>",methods=["GET"])
+def show_post  (post_id):
+   post = Post.query.get_or_404(post_id)
+   return render_template("showpost.html", post=post) 
+
+@app.route("/posts/<int:post_id>/edit",methods=["GET"])
+def show_post_edit  (post_id):
+   post = Post.query.get_or_404(post_id)
+   return render_template("postedit.html", post=post) 
+
+@app.route("/posts/<int:post_id>/edit",methods=["POST"])
+def edit_post  (post_id):
+   post = Post.query.get_or_404(post_id)
+   post.tittle = request.form['tittle']
+   post.content = request.form['content']
+   db.session.add(post)
+   db.session.commit()
+   user = User.query.get_or_404(post.user_id)
+   flash(f"Post id  {post.id} has  been updated", 'success')
+   return render_template("userdetail.html", user=user)
+
+
+@app.route("/posts/<int:post_id>/delete",methods=["POST"])
+def delete_post  (post_id):
+    """Delete a Postr."""
+    post = Post.query.get_or_404(post_id)
+    user = User.query.get_or_404(post.user_id)
+    db.session.delete(post)
+    db.session.commit()
+    flash(f"Post id  {post_id} has  been deleted", 'success')
+    return render_template("userdetail.html", user=user)
--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 db = SQLAlchemy()
 def connect_db(app):
     """Connect to database."""
@@ -10,10 +11,19 @@ class User (db.Model):
 
     __tablename__ = "users"
 
-    id = db.Column(db.Integer,
-                   primary_key=True,
-                   autoincrement=True)
-    first_name = db.Column(db.String(100),
-                     nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    first_name = db.Column(db.String(100), nullable=False)
     last_name = db.Column(db.String(100), nullable=True)
     image = db.Column(db.String(400), nullable=False)
+    post = db.relationship('Post')
+
+class Post(db.Model):
+     """Post"""
+     __tablename__ = "posts"
+     id =  db.Column(db.Integer, primary_key=True,autoincrement=True)
+     tittle = db.Column(db.String(300), nullable=False)
+     content = db.Column(db.String(800), nullable=False)
+     created_at = db.Column(db.DateTime, nullable=False, server_default=db.func.now())
+     user_id = db.Column( db.Integer, db.ForeignKey('users.id'))
+     user = db.relationship('User')
+
